1b83eefb2ecca4b398656906c4398e1bad30dc9d
wangyb <wangyb@hlsteel.com>
2018-01-13 10:08:59 +0800

    增加spring初始化demo及配置

---
+++ ssm-learning/src/main/java/com/blucewang/spring/init/FirstBean.java
@@ -0,0 +1,16 @@
+package com.blucewang.spring.init;
+
+/**
+ * Created by bluceWang on 2018/1/12.
+ */
+public class FirstBean {
+    private String name = "hello world";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- ssm-learning/src/main/java/com/blucewang/spring/init/SpringInitFirst.java
+++ ssm-learning/src/main/java/com/blucewang/spring/init/SpringInitFirst.java
@@ -1,10 +1,14 @@
 package com.blucewang.spring.init;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by bluceWang on 2018/1/11.
  */
 public class SpringInitFirst {
     public static void main(String[] args) {
-
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("firstBeanFactory.xml");
+        FirstBean bean = context.getBean(FirstBean.class);
+        System.out.println(bean.getName());
     }
 }
